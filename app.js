53aa9af0410c3c6e3ecb226a3020b10e7d3a70e4
MatasMot <98148619+MatasMot@users.noreply.github.com>
2022-09-15 13:08:37 +0200

    popular and now shiwing movies JS

--- app.js
+++ app.js
@@ -26,6 +26,40 @@ document.addEventListener("DOMContentLoaded", () => {
     let mainElm = document.createElement("main")
     wrapperElm.append(mainElm)
 
+    let nowPlayingElm = document.createElement("section")
+    nowPlayingElm.setAttribute("class", "nowPlaying")
+    mainElm.append(nowPlayingElm)
+
+    let nowPlayingHeadline = document.createElement("div")
+    nowPlayingHeadline.setAttribute("class", "nowPlaying-headline")
+    nowPlayingHeadline.innerHTML = `
+        <h1>Now Showing</h1>
+        <a href="#">See more</a>
+    `
+    nowPlayingElm.append(nowPlayingHeadline)
+
+    let nowPlayingMovies = document.createElement("div")
+    nowPlayingMovies.setAttribute("class", "nowPlaying-movies")
+    nowPlayingElm.append(nowPlayingMovies)
+
+    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=c36a7c9420cf2a297763abcd5a593bf1")
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data.results)
+            data.results.forEach(movie => { 
+                let link = document.createElement("a")
+                link.setAttribute("href", `detail.html?id=${movie.id}`)
+                link.innerHTML = `
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+                    <h3>${movie.title}</h3>
+                    <p>${movie.vote_average}/10 IMDb</p>
+                    `
+                nowPlayingMovies.append(link)
+            })
+    })
+
+    /////////////////////////////////////////////////////////////
+
     let popularElm = document.createElement("section")
     popularElm.setAttribute("class", "popular")
     mainElm.append(popularElm)
@@ -34,17 +68,13 @@ document.addEventListener("DOMContentLoaded", () => {
     popularHeadline.setAttribute("class", "popular-headline")
     popularHeadline.innerHTML = `
         <h1>Popular</h1>
-        <a href="#">Show more</a>
+        <a href="#">See more</a>
     `
     popularElm.append(popularHeadline)
 
     let popularMovies = document.createElement("div")
     popularMovies.setAttribute("class", "popular-movies")
     popularElm.append(popularMovies)
-
-    /////////////////////////////////////////////////////////////
-    
-
 
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=c36a7c9420cf2a297763abcd5a593bf1")
         .then(response => response.json())
@@ -61,4 +91,5 @@ document.addEventListener("DOMContentLoaded", () => {
                 popularMovies.append(link)
             })
     })
+    
 })
